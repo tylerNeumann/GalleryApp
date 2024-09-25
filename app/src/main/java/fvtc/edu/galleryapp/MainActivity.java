a30317cbac18c7486f86a5c302fe227c43acf472
Tyler Neumann <500189307@fvtc.edu>
2024-09-25 17:37:08 -0500

    added character file and started img and character logic

--- app/src/main/java/fvtc/edu/galleryapp/MainActivity.java
+++ app/src/main/java/fvtc/edu/galleryapp/MainActivity.java
@@ -1,6 +1,9 @@
 package fvtc.edu.galleryapp;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,7 +18,70 @@ import fvtc.edu.galleryapp.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
+public static final String TAG = "MainActivity";
+    Character[] Characters = {
+            new Character("Beakon", "thunder bird"),
+            new Character("Chillet", "ice dragon"),
+            new Character("Daedream","poison fae"),
+            new Character("Foxsparks","fire fox"),
+            new Character("Helzypher", "hell dragon"),
+            new Character("Nitewing", "bird"),
+            new Character("Pyrin Noct", "dark flaming kyrin"),
+            new Character("Tombat", "bat"),
+            new Character("Univolt", "lightning unicorn")
+    };
+    int[] imgs ={
+            R.drawable.beakon,
+            R.drawable.chillet,
+            R.drawable.daedream,
+            R.drawable.foxsparks,
+            R.drawable.helzypher,
+            R.drawable.nitewing,
+            R.drawable.pyrin_noct,
+            R.drawable.tombat,
+            R.drawable.univolt,
+            R.drawable.beakon2,
+            R.drawable.chillet2,
+            R.drawable.daedream2,
+            R.drawable.foxsparks2,
+            R.drawable.helzypher2,
+            R.drawable.nitewing2,
+            R.drawable.pyrin_noct2,
+            R.drawable.tombat2,
+            R.drawable.univolt2
+    };
+    int[] textFiles= {
+            R.raw._amber,
+            R.raw._barbara,
+            R.raw._bennett,
+            R.raw._charlotte,
+            R.raw._chongyun,
+            R.raw._diona,
+            R.raw._freminet,
+            R.raw._gaming,
+            R.raw._gorou,
+            R.raw._jean,
+            R.raw._kaeya,
+            R.raw._kirara,
+            R.raw._kujou_sara,
+            R.raw._lisa,
+            R.raw._lynette,
+            R.raw._mona,
+            R.raw._ningguang,
+            R.raw._noelle,
+            R.raw._player,
+            R.raw._razor,
+            R.raw._rosaria,
+            R.raw._tighnari,
+            R.raw._xinagling,
+            R.raw._xingqiu,
+            R.raw._xinyan,
+    };
+    int cardNum = 0;
+    boolean isFront = true;
+    ImageView imgCard;
+    TextView tvCard;
+    GestureDetector gestureDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_character1, R.id.navigation_character2, R.id.navigation_character3)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
